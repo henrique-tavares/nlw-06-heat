67b9dbcd7664114b56ad18c039adac253796e991
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2021-10-19 22:51:53 -0300

    dia 01

---
+++ backend-node/src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { Get3LastMessagesController } from "./controllers/Get3LastMessagesController";
+import { ProfileUserController } from "./controllers/ProfiileUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+
+router.get("/last3", new Get3LastMessagesController().handle);
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
+
+export { router };
